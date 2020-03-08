c973089a4a1216a3acaa24f807f3de82dbf41e64
Michael <mikemikhailov2003@gmail.com>
2020-03-08 18:21:31 +0100

    Made use of localStorage token

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 import configureStore from '../store/index.store';
+import {setCurrentUser} from '../store/actions/auth.actions';
 import Navbar from './Navbar';
 import Main from './Main';
 
 const store = configureStore();
+
+if(localStorage.getItem('jwtToken') && jwtDecode(localStorage.getItem('jwtToken'))) {
+  const token = localStorage.getItem('jwtToken');
+  const { id, username, profileImageUrl} = jwtDecode(token);
+  store.dispatch(setCurrentUser({id, username, profileImageUrl, token}))
+} else {
+  store.dispatch(setCurrentUser({}))
+}
 
 const App = () => {
   return (
