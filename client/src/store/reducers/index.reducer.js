611d9bcb5180db530cc7e82a9adfb42b8eb99c12
Michael <mikemikhailov2003@gmail.com>
2020-03-09 11:13:41 +0100

    Added messages display on login

--- client/src/store/reducers/index.reducer.js
+++ client/src/store/reducers/index.reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import user from './user.reducer';
 import error from './error.reducer';
+import messages from './messages.reducer';
 
 const rootReducer = combineReducers({
   user,
   dbErrors: error,
+  messages,
 });
 
 export default rootReducer;
