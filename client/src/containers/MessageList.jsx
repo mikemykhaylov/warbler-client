7776398b172c95435849a2933191be6d7853db12
Michael <mikemikhailov2003@gmail.com>
2020-03-10 23:02:48 +0100

    Added error handling

--- client/src/containers/MessageList.jsx
+++ client/src/containers/MessageList.jsx
@@ -1,19 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Moment from 'react-moment';
-import { Feed, Button } from 'semantic-ui-react';
+import { Feed, Button, Message } from 'semantic-ui-react';
 import { fetchMessages, deleteMessage } from '../store/actions/messages.actions';
 import defaultProfileImage from '../images/defaultProfileImage.png';
 
 const MessageList = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const messages = useSelector((state) => state.messages);
   const user = useSelector((state) => state.user.user);
+  const dbErrors = useSelector((state) => state.dbErrors);
   const { token, id: userId } = user;
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchMessages(token));
   }, [token]);
+
+  let errorMessage;
+  if (dbErrors.message) {
+    errorMessage = (
+      <Message error>
+        <Message.Header>Failed to get or delete a message</Message.Header>
+        {dbErrors.message.split('\n').length > 1 ? (
+          <Message.List>
+            {dbErrors.message.split('\n').map((error) => (
+              <Message.Item key={error}>{error}</Message.Item>
+            ))}
+          </Message.List>
+        ) : (
+          <p>{dbErrors.message}</p>
+        )}
+      </Message>
+    );
+  }
+
+  async function handleDelete(messageId) {
+    setIsLoading(true);
+    await dispatch(deleteMessage(userId, messageId, token));
+    setIsLoading(false);
+  }
+
   const messageList = messages.map((message) => (
     <Feed.Event key={message.messageId}>
       <Feed.Label image={message.author.profileImageUrl || defaultProfileImage} />
@@ -28,9 +55,10 @@ const MessageList = () => {
         <Feed.Meta>
           {message.author.authorId === userId ? (
             <Button
-              color="red"
+              negative
               size="tiny"
-              onClick={() => dispatch(deleteMessage(userId, message.messageId, token))}
+              onClick={() => handleDelete(message.messageId)}
+              loading={isLoading}
             >
               Delete
             </Button>
@@ -39,7 +67,12 @@ const MessageList = () => {
       </Feed.Content>
     </Feed.Event>
   ));
-  return <Feed>{messageList}</Feed>;
+  return (
+    <Feed>
+      {errorMessage}
+      {messageList}
+    </Feed>
+  );
 };
 
 export default MessageList;
