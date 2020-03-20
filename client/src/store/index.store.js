4713d761cdf81cff7e433f49695b66117327bdd0
Michael <mikemikhailov2003@gmail.com>
2020-03-20 19:52:33 +0200

    Fixed React Devtools

--- client/src/store/index.store.js
+++ client/src/store/index.store.js
@@ -2,18 +2,18 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index.reducer';
 
-const reactMiddleware = [applyMiddleware(thunk)];
-
-if (process.env.NODE_ENV !== 'production') {
-  reactMiddleware.push(
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__({
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
         trace: true,
-      }),
-  );
-}
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+);
 
 export default function configureStore() {
-  const store = createStore(rootReducer, compose(...reactMiddleware));
+  const store = createStore(rootReducer, enhancer);
   return store;
 }
